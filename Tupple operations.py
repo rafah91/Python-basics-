de73fe4360a1448cd3f5b4965999ac884e9e060b
rafah <rafah.hawa@yahoo.fr>
2023-07-10 15:13:34 +0200

    add operation list tupple and dictionary

---
+++ Tupple operations.py
@@ -0,0 +1,3 @@
+t=(0,1,3,4,True)
+print(min(t))
+print(max(t))
---
+++ dictionnary operations.py
@@ -0,0 +1,12 @@
+d={'ahmed':70,'ali':80}
+
+print(d['ali'])
+
+
+d['ali']=90
+print(d)
+print(d.keys())
+print(d.values())
+print(d.items())
+for x,y in d.items():
+    print(x,y)
